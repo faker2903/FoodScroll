115280c41f3f3114c4e87c3765d433a479c52676
faker2903 <ace2903722@gmail.com>
2025-09-12 23:54:59 +0530

    connected db

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,17 +1,13 @@
+// backend/src/app.js (or server.js)
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-
-
-function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log("MongoDB connected");
-        })
-        .catch((err) => {
-            console.log("MongoDB connection error:", err);
-        })
-}
-
-module.exports = connectDB;
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 //connecting the database
